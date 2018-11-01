c1cd424e10244b230182e2cf4fc5e9756123b7c6
sanmeiji <cricetulu.cach@gmail.com>
2018-11-01 11:14:51 -0400

    Add files via upload

---
+++ main.R
@@ -0,0 +1,15 @@
+setwd("~/Desktop/following_accounts")
+library(tidyverse)
+
+accounts <- read_csv("following_accounts.csv")
+
+group_by(accounts, instance) %>% 
+  summarise(count = n()) %>% 
+  filter(count > 1) %>% 
+  ggplot(aes(x = fct_reorder(instance, count), y = count, label = count)) +
+  geom_bar(stat = "identity") +
+  geom_text(size = 4, position = position_stack(vjust = 0.5), color = "white") +
+  labs(x = "", y = "", title = "Counts of following accounts per instance",
+       subtitle = "Instance with only one following account omitted") +
+  theme(axis.text.y = element_text(size = 14)) +
+  coord_flip()
